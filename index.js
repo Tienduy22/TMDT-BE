46d2039b2845020d7b682b113484d529c4999f19
Hà Tiến Duy <166206834+Tienduy22@users.noreply.github.com>
2025-06-10 15:18:40 +0700

    Fix: use process.env.PORT for Render deploy

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const routeClient = require("./api/v1/routes/client/index.route");
 database.connect();
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(cookieParser());
